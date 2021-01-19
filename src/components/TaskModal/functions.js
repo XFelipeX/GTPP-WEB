83eb5ab4bf7da881c3430f37f332046f47052dcb
Felipe Dias <felipediasamorimpessoa@gmail.com>
2021-01-19 17:11:28 -0300

    mudancas modal da tarefa

--- src/components/TaskModal/functions.js
+++ src/components/TaskModal/functions.js
@@ -1,28 +1,92 @@
-import api from '../../services/api';
+import api from "../../services/api";
+import { store } from "react-notifications-component";
 
-export const updateDescription = async (taskId, description, priority,auth) => {
-    const AUTH = auth;
-  
-    try {
-      const data = await api.put(`GTPP/Task.php?AUTH=${AUTH}&app_id=3`, {
+export const updateDescription = async (
+  taskId,
+  description,
+  priority,
+  auth
+) => {
+  const AUTH = auth;
+
+  try {
+    const data = await api
+      .put(`GTPP/Task.php?AUTH=${AUTH}&app_id=3`, {
         id: taskId,
         description: description,
-        priority:priority
+        priority: priority,
       })
       .then((response) => {
         return response;
       });
-  
-    // console.log(data);
-    } catch (error) {
-      let msg = String(error.response.data.message);
-      // console.log(error.response.data.message);
-      if(msg.includes("Only the task creator or administrator can do this")){
-        alert("Somente o criador da tarefa ou administrador pode fazer isto!");
-      }else if(msg.includes("The full description cannot be empty")){
-        alert("A descrição completa não pode estar vazia!");
-      }else if(msg.includes("Task with this state cannot be modified")){
-        alert("Tarefa neste estado não pode ser modificada!")
-      }
+
+    if (data.error === true) {
+      showNotification("Erro", String(data.message), "danger");
+      return null;
     }
+
+    showNotification("Sucesso", "A descrição da tarefa foi atualizada", "success");
+    return data;
+    // console.log(data);
+  } catch (error) {
+    let msg = String(error.response.data.message);
+    // console.log(error.response.data.message);
+    if (msg.includes("Only the task creator or administrator can do this")) {
+      showNotification(
+        "Aviso",
+        "Somente o criador da tarefa ou administrador pode fazer isto",
+        "warning"
+      );
+    } else if (msg.includes("The full description cannot be empty")) {
+      showNotification(
+        "Aviso",
+        "Preencha o campo da descrição para atualizar",
+        "warning"
+      );
+    } else if (msg.includes("Task with this state cannot be modified")) {
+      showNotification(
+        "Aviso",
+        "Tarefa neste estado não pode ser modificada",
+        "warning"
+      );
+    } else if(msg.includes("(id, full_description || (description, priority)) is broken")){
+      showNotification(
+        "Aviso",
+        "Preencha o campo da descrição para atualizar",
+        "warning"
+      );
+    }
+
+    return null;
   }
+};
+
+export async function getMessage(auth,taskId){
+  const AUTH = auth;
+
+  try {
+      const {data} = await api.get(`GTPP/Message.php?AUTH=${AUTH}&app_id=3&task_id=${taskId}`);
+      
+      // console.log(data);
+      return data.data
+  } catch (error) {
+      console.log(error);
+      return [{}]
+  }
+}
+
+export function showNotification(title, message, type) {
+  store.addNotification({
+    title: title,
+    message: message,
+    type: type,
+    container: "top-center",
+    insert: "top",
+    animationIn: ["animate__animated animate__fadeIn"],
+    animationOut: ["animate__animated animate__fadeOut"],
+    dismiss: {
+      duration: 2000,
+    },
+    width: 400,
+  });
+}
--- src/components/TaskModal/index.js
+++ src/components/TaskModal/index.js
@@ -1,48 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./style.css";
-import { updateTask, taskVisibleUpdate, getTaskFilter } from "../../redux";
+import {
+  updateTask,
+  taskVisibleUpdate,
+  getTaskFilter,
+  getWebSocket,
+  getWebSocketMessage,
+  getWebSocketHistoric,
+} from "../../redux";
 import { AiOutlineClose } from "react-icons/ai";
 import { BiEdit } from "react-icons/bi";
 import TaskTopicList from "../TaskTopicList";
 import TaskInfo from "../TaskInfo";
 import Loading from "../Loading";
-import { updateDescription } from "./functions";
+import { updateDescription, getMessage } from "./functions";
 import ModalDescription from "../ModalDescription";
-import {BsChatSquareDotsFill} from 'react-icons/bs';
-import WebChat from '../WebChat';
+import { BsChatSquareDotsFill } from "react-icons/bs";
+import WebChat from "../WebChat";
 
-let TaskModal = ({ id = "modal", close }) => {
+let TaskModal = ({ id = "modal", close, open }) => {
   const dispatch = useDispatch();
   const { taskVisible } = useSelector((state) => state);
   const { tasks } = useSelector((state) => state);
   const { permissions } = useSelector((state) => state);
+  const { webSocket } = useSelector((state) => state);
+  const { warning } = useSelector((state) => state);
+  const { modalUpdate } = useSelector((state) => state);
+  const { stateUpdate } = useSelector((state) => state);
+  const { stateAdmin } = useSelector((state) => state);
   const AUTH = permissions.session;
   const [description, setDescription] = useState(taskVisible.info.description);
   const [showDesc, setShowDesc] = useState(false);
-  const [showWebChat,setShowWebChat] = useState(false);
+  const [showWebChat, setShowWebChat] = useState(false);
+
+  // console.log(taskVisible)
 
   function upDescription(taskId, description, priority) {
-    updateDescription(taskId, description, priority, AUTH).then(() => {
-      setDescription(description);
+    updateDescription(taskId, description, priority, AUTH).then((response) => {
+      if (response !== null) {
+        setDescription(description);
+        SendInfo("A descrição simples da tarefa foi atualizada",description);
+      }
     });
     setShowDesc(false);
 
     // let changes = [...tasks];
 
-   tasks.map(task => {
-      if(task.id===taskVisible.info.task_id){
+    tasks.map((task) => {
+      if (task.id === taskVisible.info.task_id) {
         task.description = description;
-
       }
-    })
+    });
 
     // console.log(changes)
 
     // dispatch(getTaskFilter([...changes]));
   }
 
-  // console.log(taskVisible)
+  function SendInfo(msg,update) {
+    // alert("teste")
+    if (msg !== "" && webSocket.websocketState === "connected") {
+      try {
+        let jsonString = {
+          task_id: taskVisible.info.task_id,
+          message: {description:msg,task_id:taskVisible.info.task_id,update:update},
+          date_time: null,
+          user_id: Number(permissions.id),
+          type: 3,
+        };
+        webSocket.websocket.send(JSON.stringify(jsonString));
+
+        // console.log(jsonString);
+      } catch (error) {
+        alert(error);
+      }
+    }
+  }
+
   // console.log(permissions)
   const handleOutsideClick = (e) => {
     // console.log(e.target.id);
@@ -58,11 +93,67 @@ let TaskModal = ({ id = "modal", close }) => {
     setLoading(false);
   }, 1000);
 
+  const [notification, setNotification] = useState(0);
+
+  useEffect(() => {
+    if (taskVisible.info.notifications[0].amount > 99) {
+      setNotification("+99");
+    } else if (taskVisible.info.notifications[0].amount === 0) {
+      setNotification(0);
+    } else {
+      setNotification(taskVisible.info.notifications[0].amount);
+    }
+  }, [taskVisible.info.notifications[0].amount]);
+
+  const [warningState, setWarningState] = useState("");
+
+  useEffect(() => {
+    // console.log('verificando')
+    function verifyWarning() {
+      let task = warning.warning.filter(task => task.task_id == taskVisible.info.task_id);
+      // console.log(task);
+      if (warning.warning.length>0) {
+        const due_date = task[task.length-1].due_date;
+        const expire = task[task.length-1].expire;
+        const initial = task[task.length-1].initial;
+
+        // console.log(warning);
+
+        if (due_date > 0) {
+          if (due_date === 1) {
+            setWarningState("A tarefa vence hoje");
+          } else {
+            setWarningState("A tarefa venceu há " + due_date + " dias");
+          }
+        } else if (expire > 0) {
+          if (expire === 1) {
+            setWarningState("A tarefa expira em " + expire + " dia");
+          } else {
+            setWarningState("A tarefa expira em " + expire + " dias");
+          }
+        } else if (initial > 0) {
+          if (initial === 1) {
+            setWarningState("A tarefa começa em " + initial + " dia");
+          } else {
+            setWarningState("A tarefa começa em " + initial + " dias");
+          }
+        }
+      }
+    }
+
+    // setTimeout(() => {
+    //   verifyWarning();
+    // },2000)
+
+    verifyWarning();
+  }, [warning.warning.length]);
+
   return (
     <div id={id} className="modal" onClick={handleOutsideClick}>
       {loading == true ? <Loading /> : null}
       {showDesc ? (
         <ModalDescription
+          showDelete={true}
           description={description}
           setShowDesc={(info) => setShowDesc(info)}
           updateDesc={(info) =>
@@ -78,16 +169,23 @@ let TaskModal = ({ id = "modal", close }) => {
       <div className="modalContainer">
         <div className="modalHeader">
           <div className="modalTaskDescription">
-            <div onClick={() => setShowDesc(true)}>
-            <BiEdit
-                size="20"
-                
-                className="btnEdit"
-              />
-             
-            </div>
             <div>
-            {description}
+              <div onClick={() => setShowDesc(true)}>
+                <BiEdit size="20" className="btnEdit" />
+              </div>
+
+              <span>{description}</span>
+            </div>
+            <div
+              style={
+                warningState.includes("venceu") || warningState.includes("vence")
+                  ? { color: "red" }
+                  : warningState.includes("expira")
+                  ? { color: "yellow" }
+                  : {}
+              }
+            >
+              {warningState}
             </div>
           </div>
           <div>
@@ -104,23 +202,39 @@ let TaskModal = ({ id = "modal", close }) => {
         </div>
 
         <div className="modalContent">
-          <TaskInfo />
+          <TaskInfo close={(close) => close()} open={open} />
 
           <TaskTopicList />
         </div>
       </div>
 
-      <div className="webChatArea">
-          <div className="chatIcon" onClick={() =>{
-             setShowWebChat(!showWebChat)
-             
-          }}>
-          <BsChatSquareDotsFill size={50} color="white"/>
-          </div>
-          {showWebChat ===true ? (
-            <WebChat/>
-          ): null}
+      <div
+        className="chatIcon"
+        id="openChat"
+        onClick={() => {
+          getMessage(permissions.session, taskVisible.info.task_id)
+            .then((response) => dispatch(getWebSocketHistoric(response)))
+            .then(() => {});
+          setShowWebChat(!showWebChat);
+          document.getElementById("openChat").style.display = "none";
+          taskVisible.info.notifications[6].amount -=
+            taskVisible.info.notifications[0].amount;
+          taskVisible.info.notifications[0].amount = 0;
+        }}
+      >
+        <div
+          className="webChatNotification"
+          style={notification === 0 ? { backgroundColor: "transparent" } : null}
+        >
+          <div>{notification > 0 && notification}</div>
+        </div>
+        <BsChatSquareDotsFill size={50} color="white" />
       </div>
+      {showWebChat === true ? (
+        <div className="webChatArea">
+          <WebChat close={() => setShowWebChat(false)} />
+        </div>
+      ) : null}
     </div>
   );
 };
