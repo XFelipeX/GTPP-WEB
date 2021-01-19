7947fbdca5fc97e9bc899fb4932440c2a6c49c8d
Felipe Dias <felipediasamorimpessoa@gmail.com>
2021-01-19 17:06:27 -0300

    mudancas modal confirmar acao

--- src/components/ConfirmAction/index.js
+++ src/components/ConfirmAction/index.js
@@ -1,44 +1,36 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 let ConfirmAction = (props) => {
-    return (
-      <div  className="modalConfirm">
-              <div >
-             
-              <ul  className="menuConfirm">
-              <li>
-                  <h3>{props.question}?</h3>
-                </li>
-                <li>
-                  <button
-                    className="btnConfirm"
-                    onClick={(e) =>
-                      {
-                        props.confirm();
-                        props.action();
-                      }
-                    }
-                    style={{marginTop:"0"}}
-                  >
-                    Confirmar
-                  </button>
-                  <button
-                    className="btnCancel"
-                    onClick={() =>
-                      props.cancelAction()
-                    }
-                    style={{marginTop:"0"}}
-                  >
-                    Cancelar
-                  </button>
-                </li>
-              </ul>
-              </div>
-              </div>
-    )
-  }
+  return (
+    <div className="modalConfirm">
+      <div>
+        <ul className="menuConfirm">
+          <li>
+            <h3>{props.question}?</h3>
+          </li>
+          <li>
+            <button
+              className="btnConfirm"
+              onClick={(() => {
+                props.confirm(); props.action();
+              })}
+              style={{ marginTop: "0" }}
+            >
+              Confirmar
+            </button>
+            <button
+              className="btnCancel"
+              onClick={() => props.cancelAction()}
+              style={{ marginTop: "0" }}
+            >
+              Cancelar
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-
-
-  export default ConfirmAction;
+export default ConfirmAction;
