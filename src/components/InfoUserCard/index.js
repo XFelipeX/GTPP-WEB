b331848ebd520d985211b0557c0b704ace77e02a
Felipe Dias <felipediasamorimpessoa@gmail.com>
2021-01-19 17:15:09 -0300

    mudancas card do usuário

--- src/components/InfoUserCard/index.js
+++ src/components/InfoUserCard/index.js
@@ -3,7 +3,24 @@ import api from "../../services/api";
 import { useSelector } from "react-redux";
 import userEmpty from "../../assets/nullphoto.jpeg";
 import useClickOutside from '../ClickOutside';
+import { store } from "react-notifications-component";
 import './style.css';
+
+function showNotification(title, message, type) {
+  store.addNotification({
+    title: title,
+    message: message,
+    type: type,
+    container: "top-center",
+    insert: "top",
+    animationIn: ["animate__animated animate__fadeIn"],
+    animationOut: ["animate__animated animate__fadeOut"],
+    dismiss: {
+      duration: 2000,
+    },
+    width: 400,
+  });
+}
 
 let InfoUserCard = ({ id, close }) => {
   const { permissions } = useSelector((state) => state);
@@ -23,11 +40,27 @@ let InfoUserCard = ({ id, close }) => {
       let { data } = await api.get(
         "CCPP/Employee.php?AUTH=" + AUTH + "&app_id=3&id=" + userId
       );
-      //   console.log(data);
-      setInfoUser(data.data);
+      
+      if(data.error===true){
+        let msg = data.message;
+  
+        if(msg.includes('No data')){
+          // showNotification('Aviso','Você ainda não possui tarefas','warning');
+        }else if(msg.includes('Authorization denied')){
+          showNotification('Erro','Autorização negada','danger');
+        }
+        
+        else{
+          showNotification('Erro',msg,'danger');
+        }
+      }else{
+        setInfoUser(data.data);
+      }
+
+      
       // ;
     } catch (error) {
-      // console.log(error)
+      showNotification('Erro',error.message,'danger');
     }
   }
 
