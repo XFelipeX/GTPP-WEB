a38a862e89e038ca42415bce0696a16818c795fc
Felipe Dias <felipediasamorimpessoa@gmail.com>
2021-01-19 17:05:44 -0300

    mudancas filtro redux

--- src/redux/filterTasks/filterTasksActions.js
+++ src/redux/filterTasks/filterTasksActions.js
@@ -1,4 +1,11 @@
 import { GET_TASK_FILTER } from "./filterTasksTypes";
+import { SET_DO_VISIBLE } from "./filterTasksTypes";
+import { SET_DOING_VISIBLE } from "./filterTasksTypes";
+import { SET_ANALYZE_VISIBLE } from "./filterTasksTypes";
+import { SET_STOPPED_VISIBLE } from "./filterTasksTypes";
+import { SET_BLOCKED_VISIBLE } from "./filterTasksTypes";
+import { SET_DONE_VISIBLE } from "./filterTasksTypes";
+import { SET_CANCELED_VISIBLE } from "./filterTasksTypes";
 
 export const getTaskFilter = (info) => {
   return {
@@ -6,3 +13,46 @@ export const getTaskFilter = (info) => {
     payload: info,
   };
 };
+
+export const setDoVisi = (value) => {
+  return {
+    type: SET_DO_VISIBLE,
+    payload:value
+  }
+}
+export const setDoingVisi = (value) => {
+  return {
+    type: SET_DOING_VISIBLE,
+    payload:value
+  }
+}
+export const setAnalyzeVisi = (value) => {
+  return {
+    type: SET_ANALYZE_VISIBLE,
+    payload:value
+  }
+}
+export const setStoppedVisi = (value) => {
+  return {
+    type: SET_STOPPED_VISIBLE,
+    payload:value
+  }
+}
+export const setBlockedVisi = (value) => {
+  return {
+    type: SET_BLOCKED_VISIBLE,
+    payload:value
+  }
+}
+export const setDoneVisi = (value) => {
+  return {
+    type: SET_DONE_VISIBLE,
+    payload:value
+  }
+}
+export const setCanceledVisi = (value) => {
+  return {
+    type: SET_CANCELED_VISIBLE,
+    payload:value
+  }
+}
--- src/redux/filterTasks/filterTasksReducer.js
+++ src/redux/filterTasks/filterTasksReducer.js
@@ -1,12 +1,66 @@
 import { GET_TASK_FILTER } from "./filterTasksTypes";
+import { SET_DO_VISIBLE } from "./filterTasksTypes";
+import { SET_DOING_VISIBLE } from "./filterTasksTypes";
+import { SET_ANALYZE_VISIBLE } from "./filterTasksTypes";
+import { SET_STOPPED_VISIBLE } from "./filterTasksTypes";
+import { SET_BLOCKED_VISIBLE } from "./filterTasksTypes";
+import { SET_DONE_VISIBLE } from "./filterTasksTypes";
+import { SET_CANCELED_VISIBLE } from "./filterTasksTypes";
 
-const initialState = [];
+const initialState = {
+  do: true,
+  doing: true,
+  analyze: true,
+  stopped: true,
+  blocked: true,
+  done: false,
+  canceled: false,
 
+  filter: [],
+};
 const filterTasksReducer = (state = initialState, action) => {
   // console.log(action)
   switch (action.type) {
     case GET_TASK_FILTER:
-      return action.payload;
+      return {
+        ...state,
+        filter: action.payload,
+      };
+    case SET_DO_VISIBLE:
+      return {
+        ...state,
+        do: action.payload,
+      };
+    case SET_DOING_VISIBLE:
+      return {
+        ...state,
+        doing: action.payload,
+      };
+    case SET_ANALYZE_VISIBLE:
+      return {
+        ...state,
+        analyze: action.payload,
+      };
+    case SET_STOPPED_VISIBLE:
+      return {
+        ...state,
+        stopped: action.payload,
+      };
+    case SET_BLOCKED_VISIBLE:
+      return {
+        ...state,
+        blocked: action.payload,
+      };
+    case SET_DONE_VISIBLE:
+      return {
+        ...state,
+        done: action.payload,
+      };
+    case SET_CANCELED_VISIBLE:
+      return {
+        ...state,
+        canceled: action.payload,
+      };
     default:
       return state;
   }
